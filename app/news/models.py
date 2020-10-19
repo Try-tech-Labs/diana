f8cf9e8654a6ebbd1768fb47c7e5f450a8ce111f
Lucas Mauro <lucasmaurodesouza@gmail.com>
2020-10-19 16:11:30 -0300

    Create Tweet model

--- app/news/models.py
+++ app/news/models.py
@@ -14,7 +14,27 @@ class PostCategory(BaseModel):
     description = models.CharField(max_length=255)
 
 
-class Post(BaseModel):
+class PostMetaModel(models.Model):
     category = models.ForeignKey(to=PostCategory, on_delete=models.CASCADE)
     search_count = models.PositiveIntegerField(default=0)
     date = models.DateTimeField(default=timezone.now)
+
+    class Meta:
+        abstract = True
+
+
+class Post(BaseModel, PostMetaModel):
+    pass
+
+
+class Tag(BaseModel):
+    pass
+
+
+class Tweet(BaseModel, PostMetaModel):
+    post = models.ForeignKey(to=Post, on_delete=models.CASCADE)
+    content = models.TextField()
+    author = models.CharField(max_length=255)
+    tags = models.ManyToManyField(Tag)
+    source = models.CharField(max_length=255)
+    source_url = models.URLField()
