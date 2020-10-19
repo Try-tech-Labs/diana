0fb29d09a4f694a5b955f640ad501e1dd8ef81f0
Lucas Mauro <lucasmaurodesouza@gmail.com>
2020-10-19 13:31:31 -0300

    Create Post entity

--- app/news/models.py
+++ app/news/models.py
@@ -1,7 +1,20 @@
 from django.db import models
+from django.utils import timezone
 
 
 class BaseModel(models.Model):
-    id = models.IntegerField(primary_key=True)
+    id = models.PositiveIntegerField(primary_key=True)
     title = models.CharField(max_length=255)
+
+    class Meta:
+        abstract = True
+
+
+class PostCategory(BaseModel):
     description = models.CharField(max_length=255)
+
+
+class Post(BaseModel):
+    category = models.ForeignKey(to=PostCategory, on_delete=models.CASCADE)
+    search_count = models.PositiveIntegerField(default=0)
+    date = models.DateTimeField(default=timezone.now)
