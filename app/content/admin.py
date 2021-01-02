ac82470fee053101650672a99a98e4438a9aa498
Lucas Mauro <lucasmaurodesouza@gmail.com>
2021-01-02 00:23:55 -0300

    Add admin page for Post Category

--- app/content/admin.py
+++ app/content/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 
 from .models import Post
+from .models import PostCategory
 
 
 class PostAdmin(admin.ModelAdmin):
@@ -8,4 +9,10 @@ class PostAdmin(admin.ModelAdmin):
     search_fields = ('id', 'title', 'category__title', 'search_count', 'creation_date')
 
 
+class PostCategoryAdmin(admin.ModelAdmin):
+    list_display = ('id', 'title', 'description')
+    search_fields = ('id', 'title', 'description')
+
+
 admin.site.register(Post, PostAdmin)
+admin.site.register(PostCategory, PostCategoryAdmin)
