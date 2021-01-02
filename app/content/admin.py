e00c8f5fb3c344f3cf45e79066e649d00284a171
Lucas Mauro <lucasmaurodesouza@gmail.com>
2021-01-02 00:24:52 -0300

    Add admin page for Tweet

--- app/content/admin.py
+++ app/content/admin.py
@@ -3,6 +3,7 @@ from django.contrib import admin
 from .models import Post
 from .models import PostCategory
 from .models import Tag
+from .models import Tweet
 
 
 class PostAdmin(admin.ModelAdmin):
@@ -20,6 +21,13 @@ class TagAdmin(admin.ModelAdmin):
     search_fields = ('id', 'title')
 
 
+class TweetAdmin(admin.ModelAdmin):
+    list_display = ('id', 'title', 'category', 'source', 'author', 'creation_date')
+    list_filter = ('author', 'tags')
+    search_fields = ('id', 'title', 'category__title', 'source', 'author', 'creation_date')
+
+
 admin.site.register(Post, PostAdmin)
 admin.site.register(PostCategory, PostCategoryAdmin)
 admin.site.register(Tag, TagAdmin)
+admin.site.register(Tweet, TweetAdmin)
